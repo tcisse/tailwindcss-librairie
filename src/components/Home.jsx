54b2312a893855d25284ef69df9d424fd8e7a001
Cissé <tairou.cisse@axazara.com>
2022-11-14 11:28:20 +0100

    setup home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,30 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Home() {
-  return <div>Bienvenue</div>;
+  return (
+    <div>
+      <div className="">
+        <h1 className="text-4xl font-semibold flex items-center justify-center my-20 text-[#02c3f4]">
+          TailwindCss Libraire
+        </h1>
+
+        <span className="flex justify-center text-4xl">
+          Liste de mes composants
+        </span>
+      </div>
+      <div className="mt-5">
+        <ul className="space-x-10 mx-10 text-lg font-extrabold">
+          <Link className="hover:text-blue-500" to="/footer">
+            Footer
+          </Link>
+          <Link className="hover:text-blue-500" to="/navbar">
+            NavBar
+          </Link>
+          <Link className="hover:text-blue-500" to="/signup">
+            SignUp
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
