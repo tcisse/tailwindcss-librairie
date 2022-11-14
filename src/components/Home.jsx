0e6d0a22e90d430c385add5db8e36ea73f18a257
Cissé <tairou.cisse@axazara.com>
2022-11-14 13:36:39 +0100

    my navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,13 +11,13 @@ export default function Home() {
           Liste de mes composants
         </span>
       </div>
-      <div className="mt-5">
+      <div className="mt-5 flex justify-center">
         <ul className="space-x-10 mx-10 text-lg font-extrabold">
-          <Link className="hover:text-blue-500" to="/footer">
-            Footer
-          </Link>
           <Link className="hover:text-blue-500" to="/navbar">
             NavBar
+          </Link>
+          <Link className="hover:text-blue-500" to="/footer">
+            Footer
           </Link>
           <Link className="hover:text-blue-500" to="/signup">
             Sign Up
