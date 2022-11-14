6770bf4c6c3bea4c26f8587f1795fe05051ae877
Cissé <tairou.cisse@axazara.com>
2022-11-14 09:20:48 +0100

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import Footer from "./components/Footer/index";
+import Navbar from "./components/Navbar/index";
+import SignIn from "./components/SignIn/index";
+import SignUp from "./components/SignUp/index";
+import TeamSection from "./components/TeamSection/index";
+
 export default function App() {
   return (
-    <h1 className="text-3xl flex justify-center font-bold">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <Footer />
+      <Navbar />
+      <SignIn />
+      <SignUp />
+      <TeamSection />
+    </div>
+  );
 }
--- src/components/TeamSection/index.jsx
+++ src/components/TeamSection/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function index() {
-  return <div>Navbar</div>;
+  return <div>Team section</div>;
 }
