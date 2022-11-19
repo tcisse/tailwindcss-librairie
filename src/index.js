e3fbb23797983019298430c8e6a287b3ecb0640c
monlibo <libertliboo@gmail.com>
2022-11-19 11:08:37 +0100

    Remove font Poppins and ThemeProvider

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./utils/context/ThemeProvider";
-import ThemeToggler from "./components/ThemeToggler";
+
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <ThemeProvider>
-      <ThemeToggler />
-      <App />
-    </ThemeProvider>
+    <App />
   </BrowserRouter>
 );
 
