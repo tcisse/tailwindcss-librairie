e2237658a5f53f4388f9d73dd7edd2e5c875b623
monlibo <libertliboo@gmail.com>
2022-11-26 13:00:49 +0100

    Basic design for Footer3

--- src/components/Footer3.jsx
+++ src/components/Footer3.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer3() {
-  return <div>Footer 3</div>;
+  return (
+    <footer className="w-full py-6 bg-white shadow-md px-8 flex">
+      <div className="text-[25px] text-indigo-500">Logo</div>
+      <div className="flex items-center flex-1 justify-center">
+        <ul className="flex items-center space-x-6 ">
+          <li>© 2022 My app</li>
+          <li>All rights reserved</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Terms & Privacy</li>
+        </ul>
+      </div>
+      <div className="flex space-x-4 items-center justify-center">
+        <Link to="">F</Link>
+        <Link to="">I</Link>
+        <Link to="">T</Link>
+      </div>
+    </footer>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-    fontFamily:{
-      raleway: ["Raleway"]
-    }
+    fontFamily: {
+      raleway: ["Raleway"],
+    },
   },
   plugins: [],
-}
+};
