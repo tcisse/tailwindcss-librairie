11564e28ddba14f51cf02cfc1f7f5be7e96bf634
Cissé <tairou.cisse@axazara.com>
2022-11-30 11:25:57 +0100

    - create a newsletter link

---
+++ src/components/Newsletter/Newsletter4.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Newsletter4() {
+  return <div>Newsletter4</div>;
+}
--- src/components/Newsletter/index.jsx
+++ src/components/Newsletter/index.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function index() {
   return (
-    <div>
+    <div className="text-2xl pt-10 mx-5 font-raleway font-bold">
       <h1>Newletter component</h1>
-      <div>
-        <Link to="/newsletter1"></Link>
-        <Link to="/newsletter2"></Link>
-        <Link to="/newsletter3"></Link>
-        <Link to="/newsletter4"></Link>
+      <div className="flex justify-center mt-10 space-x-10">
+        <Link to="/newsletter1">Newsletter 1</Link>
+        <Link to="/newsletter2">Newsletter 2</Link>
+        <Link to="/newsletter3">Newsletter 3</Link>
+        <Link to="/newsletter4">Newsletter 4</Link>
       </div>
     </div>
   );
