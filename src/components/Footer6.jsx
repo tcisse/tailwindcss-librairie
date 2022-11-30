19a82d9a06a3fb6941e9f80e6b96946f7faed1b7
Cissé <tairou.cisse@axazara.com>
2022-11-30 11:08:56 +0100

    - fix footer6 styles

--- src/components/Footer6.jsx
+++ src/components/Footer6.jsx
@@ -6,28 +6,28 @@ export default function Footer6() {
       <footer class="p-4 bg-white rounded-lg shadow flex flex-col md:items-center md:justify-between md:p-6 dark:bg-gray-800">
         <ul className="flex flex-wrap justify-center  items-center mt-3 text-2xl font-bold text-gray-500 dark:text-gray-400 sm:mt-0 lg:space-x-10">
           <li>
-            <a href="/" className="mr-4 hover:underline md:mr-6 ">
+            <a href="/" className="mr-4 hover:text-blue-400 md:mr-6 ">
               About
             </a>
           </li>
           <li>
-            <a href="/" className="mr-4 hover:underline md:mr-6">
+            <a href="/" className="mr-4 hover:text-blue-400 md:mr-6">
               Privacy Policy
             </a>
           </li>
           <li>
-            <a href="/" className="mr-4 hover:underline md:mr-6">
+            <a href="/" className="mr-4 hover:text-blue-400 md:mr-6">
               Licensing
             </a>
           </li>
           <li>
-            <a href="/" className="hover:underline">
+            <a href="/" className="hover:text-blue-400">
               Contact
             </a>
           </li>
         </ul>
-        <div className="mt-5">
-          <span class="text-xlsd text-gray-500 sm:text-center dark:text-gray-400">
+        <div className="mt-5 flex items-center justify-center">
+          <span class="text-xl text-gray-500 sm:text-center dark:text-gray-400">
             © 2022{" "}
             <a href="/" class="hover:underline">
               Cisse™
