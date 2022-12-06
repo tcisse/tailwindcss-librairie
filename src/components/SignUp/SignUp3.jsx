163a943792c58c62031e12a518fc98c3fe3253af
Cissé <tairou.cisse@axazara.com>
2022-12-06 10:39:47 +0100

    - install lottiefille animation

--- src/components/SignUp/SignUp3.jsx
+++ src/components/SignUp/SignUp3.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function SignUp3() {
   return (
-    <div>SignUp3</div>
-  )
+    <div className="grid grid-cols-2">
+      <div className="">
+        <lottie-player
+          src="https://assets5.lottiefiles.com/packages/lf20_pprxh53t.json"
+          background="transparent"
+          speed="1"
+          // style="width: 300px; height: 300px;"
+          loop
+          controls
+          autoplay
+        ></lottie-player>
+      </div>
+      <div>
+        <label  for="email">Hello</label>
+        <input type="email" name="email" id="email" placeholder="email" width={50} height={20} />
+      </div>
+    </div>
+  );
 }
