ef75e818c7117e14d26fc0ff7492d1599b7483be
Ikuol <damienssopolo@gmail.com>
2022-12-09 13:53:55 +0000

    review of signup

--- src/components/SignUp/SignUp1.jsx
+++ src/components/SignUp/SignUp1.jsx
@@ -11,7 +11,7 @@ const SignUp1 = () => {
     };
 
   return (
-        <div className='flex flex-col items-center rounded-[2px] w-[300px] lg:max-w-[400px] h-[500px] bg-slate-300 mt-10 m-auto'>
+        <div className='flex flex-col items-center rounded-[2px] w-[300px] lg:w-[400px] h-[500px] bg-slate-300 mt-10 m-auto'>
             <div className='flex flex-col items-center justify-center text-center'>
                 <span className='mt-8'> <img src={github} className='w-[35px]' alt='error' /></span>
                 <h4 className='mt-3 font-medium'>Sign Up to Github</h4>
@@ -32,15 +32,15 @@ const SignUp1 = () => {
                         <div className='flex mb-3'>
                             <input type="password" className='w-[250px] lg:w-[350px] h-[50px] font-[400] leading-[1.5] border-[black] rounded-[2px] outline-none p-3' placeholder='Repeat Password' required />
                         </div>
-                        <div className='flex gap-x-3 mb-4 items-center'>
+                        <div className='flex gap-x-3 mt-8 mb-4 w-[200px] h-[10px] items-center'>
                             {checked === true ? (
-                                <BsCheck size={27} className='cursor-pointer mt-[8px]' onClick={() => setChecked(false)}/>
+                                <BsCheck size={27} className='cursor-pointer' onClick={() => setChecked(false)}/>
                                 ) : 
                                 (
-                                <input size={27} type="checkbox" className='border-none cursor-pointer mt-[0.4rem] focus:rotate-45 transition duration-100' onClick={handleChecked} />
+                                <input size={27} type="checkbox" className='border-none cursor-pointer focus:rotate-45 transition duration-100' onClick={handleChecked} />
                                 )
                             }
-                            <label className='mt-2'>I agree to all <a href='/' className='text-[#2cabe3]'>Terms</a></label>
+                            <label className=''>I agree to all <a href='/' className='text-[#2cabe3]'>Terms</a></label>
                         </div>
                         <div className='flex items-stretch'>
                             <button className='w-full mt-3 h-[40px] bg-black text-white hover:scale-90 transform transition duration-100'>Sign Up</button>
