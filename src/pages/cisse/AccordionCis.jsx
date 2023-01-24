1deff56b034b527875275eaec1a0e09b7392cdfb
Cissé <tairou.cisse@axazara.com>
2023-01-24 23:44:03 +0100

    - update accordion page

--- src/pages/cisse/AccordionCis.jsx
+++ src/pages/cisse/AccordionCis.jsx
@@ -1,26 +1,68 @@
-import React, { useState } from "react";
-import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
+/* This example requires Tailwind CSS v3.0+ */
+import { Disclosure } from "@headlessui/react";
+// import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BiMinus } from "react-icons/bi";
+
+const faqs = [
+    {
+        question: "Question title",
+        answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
+    },
+    {
+        question: "Question title",
+        answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
+    },
+    {
+        question: "Question title",
+        answer: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius iusto nobis fugiat, sapiente, laudantium laborum perspiciatis voluptatibus maxime molestias quia ad doloribus sequi illum reiciendis quaerat cupiditate aut et aliquam?"
+    }
+];
 
 export default function AccordionCis() {
-    const [expand, setExpand] = useState(false);
-    const expandClass = expand ? "display" : "hidden";
-    const ansClass = `${expandClass} p-4`;
     return (
-        <div className="shadow rounded border border-gray-100 border-t-0">
-            <div className="p-4 text-xl relative font-medium">
-                <div className="w-5/6">Are you cat</div>
-                <button
-                    aria-label="question-expander"
-                    className="absolute text-xl top-0 right-0 focus:outline-none"
-                    onClick={() => setExpand(!expand)}
-                >
-                    {expand ? <AiOutlineUp size={20} /> : <AiOutlineDown size={20} />}
-                </button>
-            </div>
-            <div className={ansClass}>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam provident soluta
-                natus illo aspernatur voluptatem totam voluptatum fuga illum accusamus nesciunt
-                architecto repudiandae distinctio quod sed, earum doloribus obcaecati delectus!
+        <div className="bg-white">
+            <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:py-40 lg:px-8">
+                <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
+                    <h2 className="text-3xl text-center font-bold leading-10 tracking-tight text-gray-900">
+                        Frequently asked questions
+                    </h2>
+                    <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
+                        {faqs.map(faq => (
+                            <Disclosure as="div" key={faq.question} className="pt-6">
+                                {({ open }) => (
+                                    <>
+                                        <dt>
+                                            <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+                                                <span className="text-base font-semibold leading-7">
+                                                    {faq.question}
+                                                </span>
+                                                <span className="ml-6 flex h-7 items-center">
+                                                    {open ? (
+                                                        <AiOutlinePlus
+                                                            className="h-6 w-6"
+                                                            aria-hidden="true"
+                                                        />
+                                                    ) : (
+                                                        <BiMinus
+                                                            className="h-6 w-6"
+                                                            aria-hidden="true"
+                                                        />
+                                                    )}
+                                                </span>
+                                            </Disclosure.Button>
+                                        </dt>
+                                        <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                                            <p className="text-base leading-7 text-gray-600">
+                                                {faq.answer}
+                                            </p>
+                                        </Disclosure.Panel>
+                                    </>
+                                )}
+                            </Disclosure>
+                        ))}
+                    </dl>
+                </div>
             </div>
         </div>
     );
